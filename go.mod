6a544e60b5f68047474c9f9bac029cc0c05a8121
Samuel Ralak <thesamuelralak@gmail.com>
2021-04-16 04:40:47 +0300

    refactor tests and add more test cases

--- go.mod
+++ go.mod
@@ -15,5 +15,6 @@ require (
 	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
 	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
 	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,37 +1,88 @@
-package tests
+package main_test
 
 import (
 	"github.com/samuelralak/goprime/routes"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-var router = routes.SetupRouter()
+var serverURL string
+
+type responseBody struct {
+	statusCode int
+	body       []byte
+}
+
+func TestMain(m *testing.M) {
+	router := routes.SetupRouter()
+	server := httptest.NewServer(router)
+
+	serverURL = server.URL
+	defer server.Close()
+
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
 
 func TestRootRoute(t *testing.T) {
-	respRecorder := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/", nil)
-	router.ServeHTTP(respRecorder, request)
+	respBody, err := makeRequest("/")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 
-	assert.Equal(t, http.StatusOK, respRecorder.Code)
-	assert.Equal(t, "{\"message\":\"Go Prime API v0.0\"}", respRecorder.Body.String())
+	assert.Equal(t, http.StatusOK, respBody.statusCode)
+	assert.Equal(t, "Go Prime API v0.0", string(respBody.body))
 }
 
 func TestNumberInputRouteWhenInputIsValidNumber(t *testing.T) {
-	respRecorder := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/55", nil)
-	router.ServeHTTP(respRecorder, request)
+	respBody, err := makeRequest("/55")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 
-	assert.Equal(t, http.StatusOK, respRecorder.Code)
-	assert.Equal(t, "53", respRecorder.Body.String())
+	assert.Equal(t, http.StatusOK, respBody.statusCode)
+	assert.Equal(t, "53", string(respBody.body))
 }
 
-func TestNumberInputRouteWhenInputIsInvalidNumber(t *testing.T) {
-	respRecorder := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/invalid", nil)
-	router.ServeHTTP(respRecorder, request)
+func TestNumberInputRouteWhenInputIsNotNumber(t *testing.T) {
+	respBody, err := makeRequest("/invalid")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 
-	assert.Equal(t, http.StatusUnprocessableEntity, respRecorder.Code)
+	assert.Equal(t, http.StatusBadRequest, respBody.statusCode)
+}
+
+func TestNumberInputRouteWhenInputIsEqualOrLessThanTwo(t *testing.T) {
+	respBody, err := makeRequest("/2")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, respBody.statusCode)
+	assert.Equal(t, "no prime number found", string(respBody.body))
+}
+
+func makeRequest(url string) (responseBody, error) {
+	resp, err := http.Get(serverURL + url)
+	if err != nil {
+		return responseBody{}, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return responseBody{}, err
+	}
+
+	result := responseBody{
+		statusCode: resp.StatusCode,
+		body:       body,
+	}
+
+	return result, nil
 }
